787dd5dbc585c83113fa076e492e580e9f10478e
Alexander <sacha03@yandex.ru>
2024-04-20 18:00:48 +0300

    modify question in the beginning

--- core/handlers.py
+++ core/handlers.py
@@ -30,23 +30,42 @@ async def start(message: Message, state: FSMContext):
     await bot.send_message(admin_id,
                            f"Получена новая заявка от {message.from_user.full_name}. Нажмите кнопку чтобы начать диалог.",
                            reply_markup=kb.get_start)
-    await bot.send_message(client_chat_id, "Здравствуйте! Ожидайте пока к диалогу подключается менеджер.")
-    await bot.send_message(client_chat_id, "Вопрос 1", reply_markup=kb.q1)
+    await bot.send_message(client_chat_id,
+                           "Здравствуйте! Пока к диалогу подключается менеджер, ответьте на два вопроса.")
+    await bot.send_message(client_chat_id, "За какой период интересует оптимизация?", reply_markup=kb.q1)
 
 
-@router.callback_query(F.data.startswith("ans_"))
+
+@router.callback_query(F.data.startswith("q1ans_"))
 async def callbacks_num(callback: CallbackQuery, state: FSMContext):
     action = callback.data.split("_")[1]
     await callback.message.edit_reply_markup()
     if action == "1":
-        await state.update_data(question1="ans1")
+        await state.update_data(question1="1 квартал 2024")
     elif action == "2":
-        await state.update_data(question1="ans2")
+        await state.update_data(question1="Прошедшие периоды")
+    await bot.send_message(client_chat_id, "Какой примерно объём закупок требуется для оптимизации?",
+                           reply_markup=kb.q2)
+
+@router.callback_query(F.data.startswith("q2ans_"))
+async def callbacks_num(callback: CallbackQuery, state: FSMContext):
+    action = callback.data.split("_")[1]
+    await callback.message.edit_reply_markup()
+    if action == "1":
+        await state.update_data(question2="от 0 до 10 млн")
+    elif action == "2":
+        await state.update_data(question2="от 10 до 50 млн")
+    elif action == "3":
+        await state.update_data(question2="от 50 до 100 млн")
+    elif action == "4":
+        await state.update_data(question2="более 100 млн")
     await callback.answer()
     data = await state.get_data()
-    await bot.send_message(admin_id, f'Вопрос 1: ответ от {callback.from_user.full_name} - {data["question1"]}')
-    await bot.send_message(client_chat_id, "Спасибо за ответы! Менеджер скоро присоединится")
-    await callback.answer()
+    await bot.send_message(admin_id,
+                           f'ответ от {callback.from_user.full_name}\n{data["question1"]}\n{data["question2"]}')
+    await bot.send_message(client_chat_id,
+                           f'Введённые данные\n{data["question1"]}\n{data["question2"]}')
+    await bot.send_message(client_chat_id, "Спасибо за ответы! Менеджер скоро присоединится.")
 
 
 @router.callback_query(F.data == "Start dialog")
@@ -56,7 +75,7 @@ async def start_dialog(callback: CallbackQuery):
     await bot.send_message(admin_id,
                            "Диалог начат. Все, что вы напишете, будет отправлено клиенту. Нажмите кнопку чтобы закончить диалог.")
     await bot.send_message(client_chat_id, "Диалог начат. Все, что вы напишете, будет отправлено менеджеру.")
-    await callback.message.edit_reply_markup(None)
+    await callback.message.edit_reply_markup()
 
 
 @router.callback_query(F.data == "Stop dialog")
@@ -65,7 +84,7 @@ async def end_dialog(callback: CallbackQuery):
     dialog_active = False
     await bot.send_message(admin_id, "Диалог закончен.")
     await bot.send_message(client_chat_id, "Диалог закончен.")
-    await callback.message.edit_reply_markup(None)
+    await callback.message.edit_reply_markup()
 
 
 @router.message()
--- core/keyboard.py
+++ core/keyboard.py
@@ -6,5 +6,10 @@ get_start = InlineKeyboardMarkup(inline_keyboard=[
 
 stop = InlineKeyboardMarkup(
     inline_keyboard=[[InlineKeyboardButton(text='Закончить диалог', callback_data="Stop dialog")]])
-q1 = InlineKeyboardMarkup(inline_keyboard=[[InlineKeyboardButton(text='Ответ 1', callback_data="ans_1")],
-                                           [InlineKeyboardButton(text='Ответ 2', callback_data="ans_2")]])
+q1 = InlineKeyboardMarkup(inline_keyboard=[[InlineKeyboardButton(text='1 квартал 2024', callback_data="q1ans_1")],
+                                           [InlineKeyboardButton(text='Прошедшие периоды', callback_data="q1ans_2")]])
+
+q2 = InlineKeyboardMarkup(inline_keyboard=[[InlineKeyboardButton(text='от 0 до 10 млн', callback_data="q2ans_1")],
+                                           [InlineKeyboardButton(text='от 10 до 50 млн', callback_data="q2ans_2")],
+                                           [InlineKeyboardButton(text='от 50 до 100 млн', callback_data="q2ans_3")],
+                                           [InlineKeyboardButton(text='более 100 млн', callback_data="q2ans_4")]])
